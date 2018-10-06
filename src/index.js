eb40a0e655f6953bfc8a03688ca2f475644e13e1
vwtt <vikram.srirangam@outlook.com>
2018-10-06 17:52:57 +0530

    using Symbol as guid.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ let qry = {
     select: function (cols) { return this.map(r => { let p = {}; cols.map(c => p[c] = r[c]); return p; }); },
     where: function (query) { let f = new Function('c', `return ${query};`); let res = this.filter(r => f(r)); Object.assign(res, qry, agg); return res; },
     jointo: function ({ r, f }) {
-        let s = []; this.map(l => { r.forEach(rr => { if (f(l, rr)){ let n = Object.assign({}, l, rr); s.push(n); }}); }); Object.assign(s, qry, agg); return s;
+        let s = []; this.map(l => { r.forEach(rr => { if (f(l, rr)){ let n = Object.assign({}, l, rr); const {__id, ...p} = n; s.push(p); }}); }); Object.assign(s, qry, agg); return s;
     },
     on: function (qry) { let f = new Function('l', 'r', `return ${qry};`); return { "r": [...this], "f": f }; },
     groupby: function (cols) {
@@ -23,7 +23,7 @@ let qry = {
     }
 };
 let dml = {
-    insert: function (row) { row["__id"] = Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36); this.push(row); },
+    insert: function (row) { row["__id"] = Symbol(); this.push(row); },
     delete: function (row) { let i = this.findIndex(r => r["__id"] === row["__id"]); this.splice(i, 1); },
     update: function (row) { let i = this.findIndex(r => r["__id"] === row["__id"]); this[i] = Object.assign(this[i], row); }
 };
